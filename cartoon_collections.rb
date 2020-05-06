6cb939f6a7b1a6b6188b9236bb30524083dd8639
David Hutchison <dhutchis68516@gmail.com>
2020-05-06 21:39:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(word_length)
-  # Your code here
+  word_length.map {|string| string.length}
 end
 
 def find_the_cheese(cheese)
